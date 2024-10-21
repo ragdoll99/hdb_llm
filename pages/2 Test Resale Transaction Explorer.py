a4d37a7dfd2c642c9c335770adb62e5ad5c685a8
Ragdoll <35715813+ragdoll99@users.noreply.github.com>
2024-10-21 18:12:09 +0800

    d

--- pages/2 Test Resale Transaction Explorer.py
+++ pages/2 Test Resale Transaction Explorer.py
@@ -84,8 +84,9 @@ mask_town = df_hdb_selected_measure['town'].isin(hdb_town_selected)
 # creates masks for years slicer
 mask_years = df_hdb_resale['year'].between(resale_year_selected[0], resale_year_selected[1])
 
+st.write(mask_years)
 ## apply mask to the data
-df_hdb_selected_measure_filtered = df_hdb_selected_measure[mask_town]
+df_hdb_selected_measure_filtered = df_hdb_selected_measure[mask_town & mask_years]
 
 # create pivot table based on selected measure
 df_pivot = pd.pivot_table(df_hdb_selected_measure_filtered, values=column_name, index=['town'], columns=['year'], aggfunc='mean')

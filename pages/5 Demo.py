a88c323a9128b18cd043be26a6470bf8684e14de
Ragdoll <35715813+ragdoll99@users.noreply.github.com>
2024-10-25 18:33:52 +0800

    d

--- pages/5 Demo.py
+++ pages/5 Demo.py
@@ -131,3 +131,4 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "assistant", "content": response})
     st.chat_message("assistant").write(response)
 
+st.write("hello")

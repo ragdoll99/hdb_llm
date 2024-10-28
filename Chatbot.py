c3b44908d95513589619c56423af5eceede1d4b2
Ragdoll <35715813+ragdoll99@users.noreply.github.com>
2024-10-28 10:01:12 +0800

    d

--- Chatbot.py
+++ Chatbot.py
@@ -23,17 +23,18 @@ if not check_password():
 
 st.title(":blue[💬 HDB resale Intelligent Bot]")
 container = st.container(border=True)
-container.write(
-"""
+with st.expander("Important Notice")
+    container.write(
+    """
 
-IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+    IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
 
-Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+    Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
 
-Always consult with qualified professionals for accurate and personalized advice.
+    Always consult with qualified professionals for accurate and personalized advice.
 
-"""
-)
+    """
+    )
 
 st.write("Ask me anything about HDB resale transaction")
 

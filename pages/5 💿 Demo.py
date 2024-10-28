b0922d3851754629c19fc32b4092ff1a93dba4b7
Ragdoll <35715813+ragdoll99@users.noreply.github.com>
2024-10-28 10:08:22 +0800

    d

--- pages/5 💿 Demo.py
+++ pages/5 💿 Demo.py
@@ -10,6 +10,21 @@ st.set_page_config(
     page_title="💬 HDB resale Intelligent Bot"
 )
 
+st.sidebar.header('Importan Notice')
+with st.sidebar.expander("Important Notice"):
+    st.write(
+    """
+
+    IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+    Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+    Always consult with qualified professionals for accurate and personalized advice.
+
+    """
+    )
+
+
 # Do not continue if check_password is not True.  
 if not check_password():  
     st.stop()

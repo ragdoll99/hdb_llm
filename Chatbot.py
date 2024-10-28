8468eecbc244f3440e0b3526195d5eda47215256
Ragdoll <35715813+ragdoll99@users.noreply.github.com>
2024-10-28 09:55:31 +0800

    dd

--- Chatbot.py
+++ Chatbot.py
@@ -22,6 +22,18 @@ if not check_password():
 # endregion <--------- Streamlit App Configuration --------->
 
 st.title(":blue[💬 HDB resale Intelligent Bot]")
+st.expander(
+"""
+
+IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+Always consult with qualified professionals for accurate and personalized advice.
+
+"""
+)
+
 st.write("Ask me anything about HDB resale transaction")
 
 
